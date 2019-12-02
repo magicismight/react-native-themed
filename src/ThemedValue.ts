8c26e932e2f04da901ceaa8ed020be9c7ba024d7
陈阳 <chenyang@shimo.im>
2019-12-02 15:57:14 +0800

    Fix Themed.Value 类型

--- src/ThemedValue.ts
+++ src/ThemedValue.ts
@@ -1,11 +1,13 @@
-export default class ThemedValue<T extends { [name: string]: string }> {
+export default class ThemedValue<
+  T extends { [name: string]: string | undefined }
+> {
   private value: T;
 
   constructor(value: T) {
     this.value = value;
   }
 
-  public selectValue(theme: keyof T): string {
+  public selectValue(theme: keyof T): string | undefined {
     return this.value[theme];
   }
 }
