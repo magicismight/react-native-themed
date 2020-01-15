2643331f98c1f1cbaa462cfe325478194593108c
钟加仁 <zhongjiaren@shimo.im>
2020-01-15 15:18:19 +0800

    feature: add indicatorStyle for ScrollView

--- src/components/ScrollView.ts
+++ src/components/ScrollView.ts
@@ -1,22 +1,26 @@
 import { ViewStyle, ScrollView, ScrollViewProps } from 'react-native';
 import createThemedComponent, { transformValue, ThemeStyle, ThemeProps } from '../createThemedComponent';
 import { transformStyle } from './transformers';
+import ThemedValue from 'react-native-themed/src/ThemedValue';
 
 
 function transformScrollViewProps(props: ThemeProps<ViewStyle, ScrollViewProps> & {
   contentContainerStyle?: ThemeStyle<ViewStyle>;
+  indicatorStyle?: ThemedValue<{ [name: string]: string }, string>
 }, mode: string): ScrollViewProps {
   const {
     style,
     contentContainerStyle,
     endFillColor,
+    indicatorStyle,
     ...noneThemedProps
   } = props;
 
   return Object.assign(noneThemedProps, {
     style: transformStyle(style, mode),
     contentContainerStyle: transformStyle(contentContainerStyle, mode),
-    endFillColor: transformValue(endFillColor, mode)
+    endFillColor: transformValue(endFillColor, mode),
+    indicatorStyle: transformValue(indicatorStyle, mode),
   }) as ScrollViewProps;
 }
 
