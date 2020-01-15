bb772072306e010129ccc2f66269b684f43d6540
钟加仁 <zhongjiaren@shimo.im>
2020-01-15 15:22:50 +0800

    lint

--- src/components/ScrollView.ts
+++ src/components/ScrollView.ts
@@ -1,7 +1,7 @@
 import { ViewStyle, ScrollView, ScrollViewProps } from 'react-native';
 import createThemedComponent, { transformValue, ThemeStyle, ThemeProps } from '../createThemedComponent';
 import { transformStyle } from './transformers';
-import ThemedValue from 'react-native-themed/src/ThemedValue';
+import ThemedValue from '../ThemedValue';
 
 
 function transformScrollViewProps(props: ThemeProps<ViewStyle, ScrollViewProps> & {
